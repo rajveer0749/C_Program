34d704f1b2221b050bccff3fc70e4f00da366bb6
rajveer <rajveercharan0749@gmail.com>
2025-07-02 22:38:55 +0530

    Palindrome number

---
+++ integer/4.Palindrome_Number.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main(){
+    int num, original, remainder = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    original = num; 
+    while(num != 0){
+        int digit = num % 10;
+        remainder = (remainder * 10) + digit;
+        num /= 10;
+    }
+    if(original == remainder){
+        printf("%d is a Palindrome Number", original);
+    }else{
+        printf("%d is not a Palindrome Number", original);
+    }
+    return 0;
+}
